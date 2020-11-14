a3e29f74edbd8e69a15ac7b3e788a1d0fa28f2d1
ilyaOff <52914481+ilyaOff@users.noreply.github.com>
2020-11-14 21:50:51 +0300

    Start
    Самая простая программа OpenGL

---
+++ GraphicTest.cpp
@@ -0,0 +1,41 @@
+﻿//#pragma comment (lib, "opengl32.lib")//DOES NOT WORK WITHOUT THIS
+
+#include <iostream>
+
+//#include <fre>
+#include <windows.h>        // Заголовочный файл для Windows// ВАЖНО!!!!
+#include <gl\gl.h>          // Заголовочный файл для OpenGL32 библиотеки
+#include <glut.h>           // Заголовочный файл для GLu32 библиотеки
+//#include <gl\glut.h>// у меня так не работает
+
+//#include <gl\glaux.h>                     // Заголовочный файл для GLaux библиотеки
+void reshape(int w, int h)
+{
+    glClearColor(1.0f, 0.5f, 0.0f, 1.0f);
+}
+void display(void)
+{
+    glClearColor(1.0f, 0.5f, 0.0f, 1.0f);
+}
+void idle(void) 
+{
+    glClearColor(1.0f, 0.5f, 0.0f, 1.0f);
+}
+
+int main(int argc, char** argv)
+{
+    
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB);
+    glutCreateWindow("VERY SIMPLE PROGRAM");// Подпись окна
+    //ссылка на мои функции
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutIdleFunc(idle);
+
+    glClearColor(1.0f, 0.5f, 0.0f, 1.0f);
+    glutMainLoop();
+    return 0;
+}
+
+
