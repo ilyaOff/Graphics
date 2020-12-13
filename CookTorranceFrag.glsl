786c4c32bf44b964c0942178599254c2d8c00f4c
ilyaOff <52914481+ilyaOff@users.noreply.github.com>
2020-12-13 15:16:44 +0300

    Освещение по куку-торенсу

--- CookTorranceFrag.glsl
+++ CookTorranceFrag.glsl
@@ -1,17 +1,17 @@
 #version 330 core
 
-uniform vec3 kLight = vec3(0.0,0.0,1.0);
+uniform vec3 kLight = vec3(1.0,1.0,1.0);
 
 
 uniform vec4 LightColorAmbient = vec4(0.3,0.3,0.3,1.0);
 uniform vec4 LightColor = vec4(1,1,1,1.0);
 
-uniform vec4 MaterialAmbient = vec4(0.2,0.2,0.2,1.0);
-uniform vec4 MaterialDiffuse = vec4(0.8,0.8,0.8,1);
+uniform vec4 MaterialAmbient = vec4(0.4,0.4,0.1,1.0);
+uniform vec4 MaterialDiffuse = vec4(0.8,0.8,0.2,1);
 uniform vec4 MaterialSurface = vec4(0.8,0.8,0.8,1);
 
-uniform float Roughness = 0.9f;//[0,1]//шероховатость
-
+uniform float Roughness = 0.4f;//[0,1]//шероховатость
+uniform float eta = 0.50f;
 
 in vec3 Vertex;
 in vec3 normal;
@@ -22,15 +22,16 @@ in vec3 e;
 
 out vec4 color;
 
-float Geometry( float dotNE,float dotEH, float dotNH, float dotNL )
+float Geometry( float dotNE,float dotNL, float dotNH, float dotEH )
 {
 	float k = min(dotNE, dotNL);
 	k = 2*k*dotNH;	
 
-	if(dotEH < k)
-		return 1;
+	//if(dotEH < k || -dotEH > -k)
+	//	return 1;
 
 	k = k/dotEH;
+	k = min(1, k);
 	return k;
 }
 
@@ -40,35 +41,50 @@ float Beksman(float dotNH)
 	float k = dotNH*dotNH;
 	float tg2 = 1/k - 1;
 	float m2 = Roughness * Roughness;
-	k = k*k*m2;
 	tg2 = tg2/m2;
 
-	k = k* exp(-tg2);
+	k = k*k*m2;	
+
+	k =  exp(-tg2)/k;
 	return k;
 }
+float Frenel(float dotNE)
+{
+	float kNE =(1 - dotNE);
+	kNE = kNE*kNE*(1 - dotNE);
 
+	float eta2 = eta*eta;
+	float f = ((eta2-1)*(eta2-1)) / ((eta2+1)*(eta2+1));
+	f = f + (1-f)*kNE;
+	return f;
+}
 
 vec4 SurfaceColor(vec3 lightdir, vec3 n, vec3 e,vec4 lightColor, vec4 material)
 {	
 	vec3 h = normalize(lightdir+e);
 
-	float dotEH =  dot(h, e);
-	float dotNH =  dot(n, h);
+	float dotEH = dot(h, e);
+	float dotNH = dot(n, h);
+	//float dotEH = max(dot(h, e),0);
+	//float dotNH = max(dot(n, h),0);
 
 	//if(dotNH == 0)
 	//	dotNH = 1.0e-7;
 
 	float dotNE =  dot(n, e);	
 	float dotNL =  dot(lightdir, n);
-	float g = Geometry(dotNE, dotEH, dotNH, dotNL);
+	//float dotNE =  max(dot(n, e),0);	
+	//float dotNL =  max(dot(lightdir, n),0);//резкая граница
+
+
+	float g = Geometry(dotNE, dotNL, dotNH, dotEH);
 	float b = Beksman(dotNH);
+	float f = Frenel(dotNE);
+	
 
-	float kNE =(1 - dotNE);
-	kNE = kNE*kNE*(1 - dotNE);
-	vec4 resmat = material - (vec4(1,1,1,1) - material)*kNE;
+	float k = f * b* g / (4*dotNE/*dotNL*/);
 
-	resmat = resmat * g * b / (4*dotNE*dotNL);
-	return resmat;
+	return k*material*lightColor;
 }
 
 
@@ -92,12 +108,16 @@ vec4 Light(vec4 DiffuselightColor, vec3 lightdir,
 {
 	vec4 diffuse = DiffuseColor(DiffuselightColor,  lightdir, Normal, DiffuseMaterial);
 	vec4 ambient = AmbientColor(AmbientlightColor,  AmbientMaterial);
-	vec4 surface = SurfaceColor(lightdir, Normal,  e, SurfacelightColor, SurfaceMaterial);
- 
-	//if (dot(lightdir, Normal) <= 0.0)
+	vec4 surface;
+	if (dot(lightdir, Normal) <= 0.0)
 	{
-	//	surface = vec4(0.0, 0.0, 0.0, 0.0);
+		surface = vec4(0.0, 0.0, 0.0, 0.0);
 	}
+	else
+	{
+		surface = SurfaceColor(lightdir, Normal,  e, SurfacelightColor, SurfaceMaterial);
+	}
+	
 	
 	return ambient*kLight.x + diffuse * kLight.y + surface* kLight.z;
 }
